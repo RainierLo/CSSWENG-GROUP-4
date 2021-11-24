f51914f83852fc68f49fa6097df058d19228678e
RainierLo <rainier_loyola@dlsu.edu.ph>
2021-11-24 17:22:17 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const url = require('url');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require(`body-parser`);
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+dotenv.config();
+
+port = process.env.PORT;
+hostname = process.env.HOSTNAME;
+uri = process.env.URI;
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Atlas Connected!");
+});
+
+app.listen(port, hostname, function () {
+    console.log(`Server running at: `);
+    console.log(`http://` + hostname + `:` + port);
+});
+
+
+
