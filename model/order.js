5e384b0e04aca3d805f7068c19e2173901b2e347
RainierLo <rainier_loyola@dlsu.edu.ph>
2022-01-04 17:34:40 +0800

    Update order.js
    typos

--- model/order.js
+++ model/order.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    User: { 
+    User: {
         type: Schema.Types.ObjectId,
         ref: "User"
     },
