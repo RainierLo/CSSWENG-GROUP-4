1532731ad196a9bd2017b3d2836041b4c8552191
RainierLo <rainier_loyola@dlsu.edu.ph>
2022-01-23 10:18:17 +0800

    changed menu.html to menu.hbs

--- router/router.js
+++ router/router.js
@@ -18,6 +18,7 @@ app.get('/checkEmail', userController.checkEmail);
 app.get('/checkOut', authSession.checkIfLoggedIn, userController.getCheckOut);
 app.get('/getUserCart', userController.getUserCart);
 app.get('/getMenu', foodController.getMenu);
+app.get('/menu', foodController.getMenuPage);
 app.get('/menu/:itemID', foodController.getIndivItemPage);
 app.get('/admin/orders', userController.getOrderPage)
 app.get('/getOrders', userController.getOrders);
