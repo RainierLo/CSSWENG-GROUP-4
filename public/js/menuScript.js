b7b719193db6ed15a4449c42a3a9cedbf4a98550
RainierLo <rainier_loyola@dlsu.edu.ph>
2022-01-26 16:53:38 +0800

    Removed unnecessary code from menuscript.js

--- public/js/menuScript.js
+++ public/js/menuScript.js
@@ -3,22 +3,6 @@ function menuFilter() {
     document.getElementById("menuDropdown").classList.toggle("show");
 }
 
-// window.onclick = function(event) {
-//     if (!event.target.matches('.drop-filter')) {
-//         var dd = document.getElementsByClassName("drop-filter-content");
-//         var i;
-//         for (i = 0; i < dd.length; i++) {
-//             var openDropdown = dd[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
-
-
-
-
 let menu = {};
 let categories = {};
 
