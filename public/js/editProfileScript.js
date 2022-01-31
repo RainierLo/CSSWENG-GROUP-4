e87141940b9f088584ec0988c4c9226f80e3f7f9
RainierLo <rainier_loyola@dlsu.edu.ph>
2022-01-31 10:02:09 +0800

    Added functions to show the user's order in their profile

--- public/js/editProfileScript.js
+++ public/js/editProfileScript.js
@@ -1,4 +1,48 @@
+let orders = {};
+
+function getUserOrder() {
+    $.get('/getUserOrder', function (result) {
+        if (result) {
+            orders = result;
+            buildUserOrderTable(orders);
+        }
+    })
+}
+
+function buildUserOrderTable(orders) {
+    let table = $('#userOrderTable');
+    table.empty();
+    if (orders.length > 0) {
+        for (var i = 0; i < orders.length; i++) {
+            var row = `<tr class="order-row">
+            <td>${orders[i].DateOrdered}</td>
+            <td>${getOrderString(orders[i].Cart)}</td>
+            <td>₱ ${orders[i].TotalPrice}</td>
+            <td>${orders[i].Status}</td>
+            </tr>`
+            //Append the row created to the table
+            table.append(row);
+        };
+    } else {
+        $('#userOrder-Table').empty();
+        $('#emptyOrders').text('You have no orders. Order now!');
+    }
+}
+
+/* This function a concatenated string of the user's orders */
+function getOrderString(Cart) {
+    var orderStr = ''
+    Cart.map(item => {
+        var str = `${item.Quantity}x ${item.FoodName} <br/>`;
+        orderStr = orderStr.concat('', str);
+    })
+    return orderStr;
+}
+
+
+
 $(document).ready(function () {
+    getUserOrder();
     function isFilled() {
         var username = validator.trim($('#username').val());
         var password = validator.trim($('#password').val());
