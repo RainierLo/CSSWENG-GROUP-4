ff5410fb9f5c4046f13b78a9d0bfab15468a1bc6
Gian Carlo Gutierrez <gian_gutierrez@dlsu.edu.ph>
2022-02-08 08:57:09 +0800

    Updated styles.css and added validation @ reviews page

--- public/js/reviewScript.js
+++ public/js/reviewScript.js
@@ -1,5 +1,31 @@
 
 
 $(document).ready(function() {
-    
+    function isValidReview() {
+        var isValid = false;
+        var review = $('#review').val()
+        var isValidLength = validator.isLength(review, { min: 1 });
+
+        if (isValidLength) {
+            $('#error').text('');
+            isValid = true;
+        } else {
+            $('#error').text('Review should not be empty');
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    async function validateField(field, fieldname) {
+        var validReview = isValidReview()
+        if (validReview) {
+            $('#submit').prop('disabled', false);
+        } else {
+            $('#submit').prop('disabled', true);
+        }
+    }
+
+    $('#review').keyup(function () {
+        validateField($('#review'), 'Review');
+    });
 })
